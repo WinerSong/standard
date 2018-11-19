91eba46cdb2d9ec7ea1c18c4987fecf98cadf37a
宋飞燕 <songfeiyand@163.com>
2018-11-19 17:23:19 +0800

    简书注册登录页面静态页面逐渐完善，新增了4张图片

--- jianshu/js/login.js
+++ jianshu/js/login.js
@@ -13,8 +13,15 @@ signIn.click(function(){
     $("#login-in-btn").css("display","block");
     $("#login-in-signer").css("display","block");
     $("#sign-up-signer").css("display","none");
+    $(".more-signer").css("display","block");
+    $(".more-signer h6").text("社交账号登录");
     signIn.addClass('active');
     signUp.removeClass('active');
+    $(".other-login-methods").click(function(e){
+        if(e.target !==dropdown-menu){
+            dropdown-menu.hide();
+        }
+    })
 })
 signUp = $("#signUp");
 signUp.click(function(){
@@ -29,6 +36,9 @@ signUp.click(function(){
     $("#login-in-btn").css("display","none");
     $("#login-in-signer").css("display","none");
     $("#sign-up-signer").css("display","block");
+    $(".more-signer h6").text("社交账号直接注册");
+    $("#weibo-link-wrap").css("display","none");
+    $(".js-more-method").css("display","none");
     signIn.removeClass('active');
     signUp.addClass('active');
 })
